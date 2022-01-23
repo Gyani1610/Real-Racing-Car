e8d8e5edaae61770598b0ed397e18549277c61aa
Gyani1610 <gyanik.ug20.ece@nitp.ac.in>
2022-01-23 16:14:24 +0530

    upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    void  Update()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.z = -10;
+
+        transform.position = newPosition;
+    }
+
+}
---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D colInfo)
+    {
+        if(colInfo.tag=="Collidable")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
